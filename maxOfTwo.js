cbc62c0ffed5c3c9bff2d658d383e70843bf5a39
“your <alexnelson97@gmail.com>
2023-09-06 20:34:58 -0600

    initial commit

---
+++ maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 2;
+let y = 2;
+
+// Write your code below this line.
+
+if (x > y) {
+  console.log(x);
+} else if (x < y) {
+  console.log(y);
+} else if ((x = y)) {
+  console.log("The values are identical");
+}
